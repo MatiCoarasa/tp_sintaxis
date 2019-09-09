eb3458c4283ab4963176176f75033876e183ecfa
MatiCoarasa <maticoarasa0@gmail.com>
2019-09-08 21:30:25 -0300

    Ajusto funciones de listas

--- libreria.c
+++ libreria.c
@@ -1,16 +1,22 @@
 #include "libreria.h"
 
 
-void agregarNodo(char* valor, t_nodo* lista){
+char* string_substring(char* text, int start, int length) {
+	char* new_string = calloc(1, length + 1);
+	strncpy(new_string, text + start, length);
+	return new_string;
+}
+
+t_nodo* agregarNodo(char* valor, t_nodo* lista){
 
     t_nodo* nuevoNodo = (t_nodo*)malloc(sizeof(t_nodo));
 
-    nuevoNodo->valor = valor;
+    nuevoNodo->valor = strdup(valor);
     nuevoNodo->siguiente = NULL;
 
     if(lista == NULL) {
-        lista = nuevoNodo;
-        return;
+    	lista = nuevoNodo;
+        return nuevoNodo;
     }
 
     t_nodo* aux = lista;
@@ -19,38 +25,38 @@ void agregarNodo(char* valor, t_nodo* lista){
         aux = aux->siguiente;
     }
 
+    nuevoNodo->siguiente = aux->siguiente;
     aux->siguiente = nuevoNodo;
-      
+    return nuevoNodo;
+
+
 }
 
-void agregarNodoOSumarContador(char* valor, t_nodo_contador* lista){
+int valorOctalADecimal(char* octalTexto){
 
-    t_nodo_contador* nuevoNodo = (t_nodo_contador*)malloc(sizeof(t_nodo_contador));
+    //Remuevo el 0 de la constante octal.
+    char* textoDecimal = string_substring(octalTexto, 1, strlen(octalTexto));
+    //Transformo la cadena de caracteres en un entero para poder sumarlo.
+    int octalSinCero = atoi(textoDecimal);
+    free(textoDecimal);
 
-    nuevoNodo->valor = valor;
-    nuevoNodo->cantidad = 1;
-    nuevoNodo->siguiente = NULL;
-
-    if(lista == NULL){
-        lista = nuevoNodo;
-        return;
+    int i = 0;
+    int valorDecimal = 0;
+    //Transformo el valor octal a decimal.
+    while (octalSinCero != 0){
+        valorDecimal += (octalSinCero % 10) * pow(8, i++);
+        octalSinCero /= 10;
     }
 
-    t_nodo_contador* aux = lista;
+	return valorDecimal;
+}
 
-    while(aux != NULL){
-        if(strcmp(aux->valor,valor)){
-            //Caso coincide valor con elemento de lista.
-            aux->cantidad++;
-            free(nuevoNodo->valor);
-            free(nuevoNodo);
-            return;
-        } else if(aux->siguiente != NULL){
-            //Caso no coincidió valor y hay siguiente elemento para recorrer.
-            aux = aux->siguiente;
-        } else {
-            //Caso no coincidió valor y no hay más elementos para recorrer.
-            aux->siguiente = nuevoNodo;
-        }
-    }
+int main(){
+
+    t_nodo* listaPrueba = NULL;
+    char* string = strdup("hola");
+    listaPrueba = agregarNodo(string, listaPrueba);
+    printf("%s\n", listaPrueba->valor);
+    return 0;
+
 }
