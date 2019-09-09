1e765b74cd3573b05e6bbe3c17e8e0143485dfa7
MatiCoarasa <maticoarasa0@gmail.com>
2019-09-08 21:31:19 -0300

    m

--- libreria.h
+++ libreria.h
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef unsigned int t_uint;
-
-typedef struct t_nodo_contador {
-    struct t_nodo_contador* siguiente;
-    t_uint cantidad;
-    char* valor;
-} t_nodo_contador;
 
 typedef struct t_nodo {
     struct t_nodo* siguiente;
     char* valor;
 } t_nodo;
 
-void agregarNodoOSumarContador(char* valor, t_nodo_contador* lista);
-void agregarNodo(char* valor, t_nodo* lista);
+t_nodo* agregarNodo(char* valor, t_nodo* lista);
+char* string_substring(char* text, int start, int length);
+int valorOctalADecimal(char* octalTexto);
